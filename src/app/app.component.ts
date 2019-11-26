222a759683eed2d7389c9a97319c9bed6e2fe5be
sandeep <sandeep.patel8@wipro.com>
2019-11-26 12:40:02 +0530

    msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyWebsite';
+
+ mymethod(){
+   console.log("clicked");
+   var modal=document.getElementById("modal");
+   var modal_bk=document.getElementById("modal_background_color");
+    modal_bk.style.display="block";
+   modal.style.display="block";
+   
+
+ 
+ }
+
+ mymethodClose(){
+   console.log("close clicked");
+   var modal_bk=document.getElementById("modal_background_color");
+    modal_bk.style.display="none";
+   var modal=document.getElementById("modal");
+   modal.style.display="none";
+
+ 
+ }
 }
